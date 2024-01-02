45b8c9951def3ede45daad89ccc953ea9b5a972f
3B032123 <3b032123@gm.student.ncut.edu.tw>
2024-01-02 22:00:59 +0800

    儲存使用者chirps的message (經檢驗通過)

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 
 class ChirpController extends Controller
 {
@@ -31,6 +32,13 @@ class ChirpController extends Controller
     public function store(Request $request)
     {
         //
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->chirps()->create($validated);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
